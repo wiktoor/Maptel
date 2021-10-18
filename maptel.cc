d2759aef53a1759f17d0e406573ee11f2e69baf6
Wiktor Chmielewski <wc429131@students.mimuw.edu.pl>
2021-10-18 13:39:18 +0200

    Dodano funkcję maptel_delete

--- maptel.cc
+++ maptel.cc
@@ -5,20 +5,24 @@ using namespace std;
 
 using dict = unordered_map<string, string>;
 using directory = unordered_map<unsigned long, dict>;
-using id = unsigned long;
 
-directory& get_directory() {
+static directory& get_directory() {
     static directory* dir = new directory();
     return *dir;
 }
 
 unsigned long maptel_create(void) {
     directory& dir = get_directory();
-    static id gid = 0;
+    static unsigned long gid = 0;
 
     dir.emplace(gid, dict());
 
     return gid++;
+}
+
+void maptel_delete(unsigned long id) {
+    directory& dir = get_directory();
+    dir.erase(id);
 }
 
 
