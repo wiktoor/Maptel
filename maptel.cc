920d01724c8fb30062d92668ffcb9ea15b374d01
Przemysław R. Kusiak <spaemann@protonmail.com>
2021-10-18 19:17:22 +0200

    Deklaracja namespace'a również w pliku .cc

--- maptel.cc
+++ maptel.cc
@@ -18,56 +18,59 @@ static directory& get_directory() {
     return *dir;
 }
 
-unsigned long maptel_create(void) {
-    DEBUG("maptel: maptel_create()");
+namespace jnp1 {
+    unsigned long maptel_create(void) {
+        DEBUG("maptel: maptel_create()");
 
-    directory& dir = get_directory();
-    static unsigned long gid = 0;
-    dir.emplace(gid, dictionary());
+        directory& dir = get_directory();
+        static unsigned long gid = 0;
+        dir.emplace(gid, dictionary());
 
-    DEBUG("maptel: maptel_create: new map id = " << gid);
-    return gid++;
-}
-
-void maptel_delete(unsigned long id) {
-    DEBUG("maptel: maptel_delete(" << id << ")");
-
-    directory& dir = get_directory();
-    size_t cnt = dir.erase(id);
-
-    if (!cnt)
-        DEBUG("maptel: maptel_delete: nothing to delete");
-    else
-        DEBUG("maptel: maptel_delete: map " << id << "deleted");
-}
-
-void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
-    if (tel_src == NULL || tel_dst == NULL) {
-        DEBUG("maptel: maptel_insert: NULL pointer passed, skipping");
-        return;
-    } else {
-        DEBUG("maptel: maptel_insert(" << id << ", " << tel_src << ", " << tel_dst << ")");
+        DEBUG("maptel: maptel_create: new map id = " << gid);
+        return gid++;
     }
 
-    dictionary& dict = get_directory()[id];
-    dict.emplace(tel_src, tel_dst);
+    void maptel_delete(unsigned long id) {
+        DEBUG("maptel: maptel_delete(" << id << ")");
 
-    DEBUG("maptel: maptel_insert: inserted");
-}
+        directory& dir = get_directory();
+        size_t cnt = dir.erase(id);
 
-void maptel_erase(unsigned long id, char const *tel_src) {
-    if (tel_src == NULL) {
-        DEBUG("maptel: maptel_erase: NULL pointer passed, skipping");
-        return;
-    } else {
-        DEBUG("maptel: maptel_erase(" << id << ", " << tel_src << ")");
+        if (!cnt)
+            DEBUG("maptel: maptel_delete: nothing to delete");
+        else
+            DEBUG("maptel: maptel_delete: map " << id << "deleted");
     }
 
-    dictionary& dict = get_directory()[id];
-    size_t cnt = dict.erase(tel_src);
+    void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
+        if (tel_src == NULL || tel_dst == NULL) {
+            DEBUG("maptel: maptel_insert: NULL pointer passed, skipping");
+            return;
+        } else {
+            DEBUG("maptel: maptel_insert("
+                    << id << ", " << tel_src << ", " << tel_dst << ")");
+        }
 
-    if (!cnt)
-        DEBUG("maptel: maptel_erase: nothing to erase");
-    else
-        DEBUG("maptel: maptel_erase: erased");
+        dictionary& dict = get_directory()[id];
+        dict.emplace(tel_src, tel_dst);
+
+        DEBUG("maptel: maptel_insert: inserted");
+    }
+
+    void maptel_erase(unsigned long id, char const *tel_src) {
+        if (tel_src == NULL) {
+            DEBUG("maptel: maptel_erase: NULL pointer passed, skipping");
+            return;
+        } else {
+            DEBUG("maptel: maptel_erase(" << id << ", " << tel_src << ")");
+        }
+
+        dictionary& dict = get_directory()[id];
+        size_t cnt = dict.erase(tel_src);
+
+        if (!cnt)
+            DEBUG("maptel: maptel_erase: nothing to erase");
+        else
+            DEBUG("maptel: maptel_erase: erased");
+    }
 }
