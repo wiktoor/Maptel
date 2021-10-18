7b0f32faea266992fc387250a475de2e715c3f36
Przemysław R. Kusiak <spaemann@protonmail.com>
2021-10-18 13:31:30 +0200

    Dodano funkcję maptel_create

--- maptel.cc
+++ maptel.cc
@@ -5,9 +5,21 @@ using namespace std;
 
 using dict = unordered_map<string, string>;
 using directory = unordered_map<unsigned long, dict>;
+using id = unsigned long;
 
 directory& get_directory() {
     static directory* dir = new directory();
     return *dir;
 }
+
+unsigned long maptel_create(void) {
+    directory& dir = get_directory();
+    static id gid = 0;
+
+    dir.emplace(gid, dict());
+
+    return gid++;
+}
+
+
 
