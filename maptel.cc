67f6a232d9b45c73843415a10dd0452c4c8d25c5
Przemysław R. Kusiak <spaemann@protonmail.com>
2021-10-25 13:45:06 +0200

    Dodanie anonimowego namespace

--- maptel.cc
+++ maptel.cc
@@ -14,9 +14,10 @@ using namespace std;
     static constexpr bool debug = true;
 #endif
 
-#define DEBUG(x)                                                        \
-    do {                                                                \
-        if (debug) std::cerr << "maptel: maptel_" << x << '\n';         \
+#define DEBUG(x)                                         \
+    do {                                                 \
+        if (debug)                                       \
+            std::cerr << "maptel: maptel_" << x << '\n'; \
     } while(0)
 
 #define CHECK_CORRECTNESS(conditional) \
@@ -33,26 +34,28 @@ using directory = unordered_map<unsigned long, dictionary>;
 using used_t = set<string>;
 
 namespace jnp1 {
-    static directory& get_directory() {
-        static directory* dir = new directory();
-        return *dir;
-    }
-
-    static void assert_tel_is_correct(const char* tel) {
-        CHECK_CORRECTNESS(tel != nullptr);
-
-        for (size_t i = 0; i < jnp1::TEL_NUM_MAX_LEN + 1; ++i) {
-            if (tel[i] == '\0')
-                return; /* the end of string */
-            else
-                CHECK_CORRECTNESS(isdigit(tel[i]));
+    namespace {
+        directory& get_directory() {
+            static directory* dir = new directory();
+            return *dir;
         }
 
-        /* Telephone number is larger than 22 characters. */
-        CHECK_CORRECTNESS(false);
+        void assert_tel_is_correct(const char* tel) {
+            CHECK_CORRECTNESS(tel != nullptr);
+
+            for (size_t i = 0; i < jnp1::TEL_NUM_MAX_LEN + 1; ++i) {
+                if (tel[i] == '\0')
+                    return; /* the end of string */
+                else
+                    CHECK_CORRECTNESS(isdigit(tel[i]));
+            }
+
+            /* Telephone number is larger than 22 characters. */
+            CHECK_CORRECTNESS(false);
+        }
     }
 
-    static dictionary* check_get_dictionary(unsigned long id) {
+    dictionary* check_get_dictionary(unsigned long id) {
         directory& dir = get_directory();
         auto it = dir.find(id);
 
@@ -89,8 +92,7 @@ namespace jnp1 {
         assert_tel_is_correct(tel_src);
         assert_tel_is_correct(tel_dst);
 
-        DEBUG("insert(" << id << ", " << tel_src << ", "
-                << tel_dst << ")");
+        DEBUG("insert(" << id << ", " << tel_src << ", " << tel_dst << ")");
 
         dictionary* dict = check_get_dictionary(id);
         (*dict)[tel_src] = string(tel_dst);
@@ -112,31 +114,33 @@ namespace jnp1 {
             DEBUG("erase: erased");
     }
 
-    static bool transform_helper(dictionary& dict, string& tel_src_str, string &res, used_t& used) {
-        if (used.count(tel_src_str) != 0) {
-            DEBUG("transform: cycle detected");
-            return false;
+    namespace {
+        bool transform_helper(dictionary& dict, string& tel_src_str, string &res, used_t& used) {
+            if (used.count(tel_src_str) != 0) {
+                DEBUG("transform: cycle detected");
+                return false;
+            }
+
+            auto it = dict.find(tel_src_str);
+
+            if (it == dict.end()) {
+                res = tel_src_str;
+                return true;
+            }
+            else {
+                used.insert(tel_src_str);
+                return transform_helper(dict, it->second, res, used);
+            }
         }
 
-        auto it = dict.find(tel_src_str);
+        void update_dst(string& tel_src_str, char *tel_dst, size_t len) {
+            CHECK_CORRECTNESS(tel_src_str.size() + 1 <= len);
 
-        if (it == dict.end()) {
-            res = tel_src_str;
-            return true;
+            for (size_t i = 0; i < tel_src_str.size(); i++)
+                tel_dst[i] = tel_src_str[i];
+
+            tel_dst[tel_src_str.size()] = '\0';
         }
-        else {
-            used.insert(tel_src_str);
-            return transform_helper(dict, it->second, res, used);
-        }
-    }
-
-    static void update_dst(string& tel_src_str, char *tel_dst, size_t len) {
-        CHECK_CORRECTNESS(tel_src_str.size() + 1 <= len);
-
-        for (size_t i = 0; i < tel_src_str.size(); i++)
-            tel_dst[i] = tel_src_str[i];
-
-        tel_dst[tel_src_str.size()] = '\0';
     }
 
     void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len) {
